b2abf33465306a2627b75ab22b6e2e7f822a4599
zeron <tlstjrrud007@naver.com>
2021-01-06 22:21:45 +0900

    DataInOut End

---
+++ DATAINOUT/DataInOut.cpp
@@ -0,0 +1,57 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+#define DATAOUT 0
+#define DATAIN 1
+String filename = "mydata.json";
+
+int main(void) {
+#if DATAOUT == 1
+	String name = "Jane";
+	int age = 10;
+	Point pt1(100, 200);
+	vector<int> scores = { 80,90,50 };
+	Mat mat1 = (Mat_<float>(2, 2) << 1.0f, 1.5f, 2.0f, 3.2f);
+
+	FileStorage fs(filename, FileStorage::WRITE);
+
+	if (!fs.isOpened()) {
+		cerr << "File open failed" << endl;
+		return 0;
+	}
+
+	fs << "name" << name;
+	fs << "age" << age;
+	fs << "point" << pt1;
+	fs << "scores" << scores;
+	fs << "data" << mat1;
+
+	fs.release();
+#endif
+#if DATAIN == 1
+	String name;
+	int age;
+	Point pt1;
+	vector<int> scores;
+	Mat mat1;
+
+	FileStorage fs(filename, FileStorage::READ);
+
+	fs["name"] >> name;
+	fs["age"] >> age;
+	fs["point"] >> pt1;
+	fs["scores"] >> scores;
+	fs["data"] >> mat1;
+
+	fs.release();
+
+	cout << "name: " << name << endl;
+	cout << "age: " << age << endl;
+	cout << "point: " << pt1 << endl;
+	cout << "scores: " << Mat(scores).t() << endl;
+	cout << "data\n" << mat1 << endl;
+#endif
+}
